9395937db93d58851f5c475b5648e19bbd81f3a5
AymenSekhri <aymen.sekhri@live.fr>
2020-06-29 12:09:57 +0100

    FileSystem: added TAR filesystem class.

---
+++ kernel/Filesystem/tar.cpp
@@ -0,0 +1,48 @@
+#include "tar.h"
+
+TarFS::TarFS(void* tar_address) : m_tar_address((TarHeader*)tar_address)
+{
+}
+
+char* TarFS::read_file(const char* path)
+{
+	return (char*)(file_search(path) + 1);
+}
+
+size_t TarFS::get_file_size(const char* path)
+{
+	return octal_to_decimal(file_search(path)->size);
+}
+
+TarHeader* TarFS::file_search(const char* path)
+{
+	size_t path_len = strlen(path) + 1;
+	if (path_len > MAX_FILE_NAME)
+		return nullptr;
+
+	TarHeader* tar_parser = m_tar_address;
+	while (tar_parser->name[0]) {
+		if (strncmp(path, tar_parser->name, path_len)) {
+			return tar_parser;
+		}
+		uintptr_t file_end = uintptr_t(tar_parser + 1) + octal_to_decimal(tar_parser->size);
+		tar_parser = (TarHeader*)(align_to(file_end, TAR_ALIGNMENT));
+	}
+	return nullptr;
+}
+
+size_t TarFS::octal_to_decimal(const char* octal)
+{
+	size_t size = 0;
+	unsigned current_multiplier = 1;
+	for (size_t i = 12; i > 0; i--) {
+		size += (octal[i - 1] - '0') * current_multiplier;
+		current_multiplier *= 8;
+	}
+	return size;
+}
+
+inline uintptr_t TarFS::align_to(uintptr_t address, unsigned alignment)
+{
+	return address + alignment - (address % alignment);
+}
---
+++ kernel/Filesystem/tar.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Lib/stdlib.h"
+#include "utils/types.h"
+
+#define MAX_FILE_NAME      100
+#define TAR_ALIGNMENT      512
+#define TAR_OCTAL_SIZE_LEN 12
+
+struct TarHeader {                 /* byte offset */
+	char name[MAX_FILE_NAME];      /*   0 */
+	char mode[8];                  /* 100 */
+	char uid[8];                   /* 108 */
+	char gid[8];                   /* 116 */
+	char size[TAR_OCTAL_SIZE_LEN]; /* 124 */
+	char mtime[12];                /* 136 */
+	char chksum[8];                /* 148 */
+	char typeflag;                 /* 156 */
+	char linkname[100];            /* 157 */
+	char magic[6];                 /* 257 */
+	char version[2];               /* 263 */
+	char uname[32];                /* 265 */
+	char gname[32];                /* 297 */
+	char devmajor[8];              /* 329 */
+	char devminor[8];              /* 337 */
+	char prefix[155];              /* 345 */
+	                               /* 500 */
+};
+
+class TarFS
+{
+  private:
+	TarHeader* m_tar_address;
+	TarHeader* file_search(const char* path);
+	size_t octal_to_decimal(const char* octal);
+	inline uintptr_t align_to(uintptr_t address, unsigned alignment);
+
+  public:
+	TarFS(void* tar_address);
+	~TarFS();
+	char* read_file(const char* path);
+	size_t get_file_size(const char* path);
+};
