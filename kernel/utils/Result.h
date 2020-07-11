234d968cc760bd550f0aeed76637c83094f6f384
AymenSekhri <aymen.sekhri@live.fr>
2020-07-11 21:08:08 +0100

    Refactoring: Result class can now accept void or references.

--- kernel/utils/Result.h
+++ kernel/utils/Result.h
@@ -2,67 +2,116 @@
 
 class ResultError
 {
-	unsigned m_error;
-
   public:
+	const unsigned m_error;
 	ResultError(unsigned error) : m_error(error)
 	{
 	}
 	~ResultError()
 	{
 	}
-
-	inline bool is_error()
-	{
-		return m_error != 0;
-	}
-
-	inline unsigned error()
-	{
-		return m_error;
-	}
-
-  private:
 };
 
 template <typename T> class Result
 {
   private:
-	ResultError m_error;
+	const ResultError m_error;
 	T ret_value;
 
   public:
-	Result(const Result& other)
-	{
-		m_error = other.m_error;
-		ret_value = other.ret_value;
-	}
-
-	Result(ResultError&& error) : m_error(error)
+	Result(const Result& other) : m_error(other.m_error), ret_value(other.ret_value)
 	{
 	}
 
-	Result(T& result) : ret_value(result), m_error(ResultError(0))
+	Result(const ResultError& error) : m_error(error)
 	{
 	}
 
-	Result(T&& result) : ret_value(result), m_error(ResultError(0))
+	Result(const T& result) : m_error(ResultError(0)), ret_value(result)
 	{
 	}
 
-	bool is_error()
+	const bool is_error() const
 	{
-		return m_error.is_error();
+		return m_error.m_error != 0;
 	}
 
-	unsigned error()
+	const unsigned error() const
 	{
-		return m_error.error();
+		return m_error.m_error;
 	}
 
 	T& value()
 	{
 		return ret_value;
+	}
+
+	~Result()
+	{
+	}
+};
+
+template <typename T> class Result<T&>
+{
+  private:
+	const ResultError m_error;
+	T* ret_value;
+
+  public:
+	Result(const Result& other) : m_error(other.m_error), ret_value(other.ret_value)
+	{
+	}
+
+	Result(const ResultError& error) : m_error(error)
+	{
+	}
+
+	Result(T& result) : m_error(ResultError(0)), ret_value(&result)
+	{
+	}
+
+	bool is_error() const
+	{
+		return m_error.m_error != 0;
+	}
+
+	unsigned error() const
+	{
+		return m_error.m_error;
+	}
+
+	T& value()
+	{
+		return *ret_value;
+	}
+
+	~Result()
+	{
+	}
+};
+
+template <> class Result<void>
+{
+  private:
+	const ResultError m_error;
+
+  public:
+	Result(const Result& other) : m_error(other.m_error)
+	{
+	}
+
+	Result(const ResultError& error) : m_error(error)
+	{
+	}
+
+	bool is_error() const
+	{
+		return m_error.m_error != 0;
+	}
+
+	unsigned error() const
+	{
+		return m_error.m_error;
 	}
 
 	~Result()
