88246eea28492047d3c18b788dadf0d104338309
AymenSekhri <aymen.sekhri@live.fr>
2020-09-10 18:22:22 +0100

    Build: use Unix Makefiles intead of VS in travis

--- Toolchain.i686-pe.cmake
+++ Toolchain.i686-pe.cmake
@@ -12,7 +12,7 @@ SET(CMAKE_CXX_COMPILER_TARGET ${triple})
 
 SET(CMAKE_C_FLAGS  "--target=i686-unknown-windows -Wall -Wextra -ffreestanding")
 SET(CMAKE_CXX_FLAGS  "--target=i686-unknown-windows -Wall -Wextra -ffreestanding -fno-exceptions -fno-rtti")
-SET(CMAKE_EXE_LINKER_FLAGS_INIT  "--target=i686-unknown-windows -nostdlib -Wl,-entry:main  -Wl,-subsystem:native")
+SET(CMAKE_EXE_LINKER_FLAGS_INIT  "--target=i686-unknown-windows -nostdlib -Wl,-entry:main")
 
 SET(CMAKE_CXX_STANDARD 17)
 SET(CMAKE_CXX_STANDARD_REQUIRED ON)
