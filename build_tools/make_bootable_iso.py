d2996a262545e99f843f60f9116c662d9ea5086c
AymenSekhri <aymen.sekhri@live.fr>
2020-12-20 10:56:27 +0100

    Meta: Refactoring.

--- build_tools/make_bootable_iso.py
+++ build_tools/make_bootable_iso.py
@@ -13,60 +13,59 @@ from pathlib import Path
 
 
 def main():
-	if(len(sys.argv) != 5):
-		print("invalid argument list")
-		sys.exit(2)
-	krnl_bin = sys.argv[1]
-	apps_bin_dir = sys.argv[2]
-	root_dir = sys.argv[3]
-	iso_out = sys.argv[4]
-	if os.path.exists(root_dir) == True:
-		shutil.rmtree(root_dir)
-	make_root(krnl_bin, root_dir)
-	create_ramdisk(apps_bin_dir, os.path.join(root_dir, "ramdisk.tar"))
-	make_iso(root_dir, iso_out)
+    if(len(sys.argv) != 5):
+        print("invalid argument list")
+        sys.exit(2)
+    krnl_bin = sys.argv[1]
+    apps_bin_dir = sys.argv[2]
+    root_dir = sys.argv[3]
+    iso_out = sys.argv[4]
+    if os.path.exists(root_dir) == True:
+        shutil.rmtree(root_dir)
+    make_root(krnl_bin, root_dir)
+    create_ramdisk(apps_bin_dir, os.path.join(root_dir, "ramdisk.tar"))
+    make_iso(root_dir, iso_out)
 
 
 def make_iso(folder_name, out_path):
-	print(platform.system())
-	if platform.system() == "Windows":
-		folder_name = os.path.relpath(folder_name)
-		folder_name = folder_name.replace(os.sep, '/')
-		out_path = out_path.replace(os.sep, '/')
-		os.system("wsl grub-mkrescue -o " + out_path + " " + folder_name)
-	elif platform.system() == "Linux":
-		os.system("grub-mkrescue -o " + out_path + " " + folder_name)
-	else:
-		print("Not supported system")
-		sys.exit(2)
+    if platform.system() == "Windows":
+        folder_name = os.path.relpath(folder_name)
+        folder_name = folder_name.replace(os.sep, '/')
+        out_path = out_path.replace(os.sep, '/')
+        os.system("wsl grub-mkrescue -o " + out_path + " " + folder_name)
+    elif platform.system() == "Linux":
+        os.system("grub-mkrescue -o " + out_path + " " + folder_name)
+    else:
+        print("Not supported system")
+        sys.exit(2)
 
 
 def make_root(bin_path, out_dir):
-	os.makedirs(out_dir)
-	os.makedirs(os.path.join(out_dir, "boot/grub/"))
-	make_grub_cfg(os.path.join(out_dir, "boot/grub/grub.cfg"),
-				  "/boot/kernel.img")
-	shutil.copyfile(bin_path,
-					os.path.join(out_dir, "boot/kernel.img"))
+    os.makedirs(out_dir)
+    os.makedirs(os.path.join(out_dir, "boot/grub/"))
+    make_grub_cfg(os.path.join(out_dir, "boot/grub/grub.cfg"),
+                  "/boot/kernel.img")
+    shutil.copyfile(bin_path,
+                    os.path.join(out_dir, "boot/kernel.img"))
 
 
 def create_ramdisk(input_path, output_path):
-	tar = tarfile.open(output_path, "w")
-	for filename in os.listdir(input_path):
-		tar.add(os.path.join(input_path, filename), filename)
-	tar.close()
+    tar = tarfile.open(output_path, "w")
+    for filename in os.listdir(input_path):
+        tar.add(os.path.join(input_path, filename), filename)
+    tar.close()
 
 
 def make_grub_cfg(cfg_path, kernel_path):
 
-	f = open(cfg_path, "w")
-	f.write('set timeout=0\n')
-	f.write('set default=0\n')
-	f.write('menuentry "CyanOS" {\n')
-	f.write('        multiboot2 ' + kernel_path + '\n')
-	f.write('        module2 /ramdisk.tar ramdisk\n')
-	f.write('}')
+    f = open(cfg_path, "w")
+    f.write('set timeout=0\n')
+    f.write('set default=0\n')
+    f.write('menuentry "CyanOS" {\n')
+    f.write('        multiboot2 ' + kernel_path + '\n')
+    f.write('        module2 /ramdisk.tar ramdisk\n')
+    f.write('}')
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- ztest/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "PathParser.h"
-#include <assert.h>
-#include <stdio.h>
-#include <cstring>
-#define ASSERT(x) assert(x)
-int main()
-{
-	return 0;
-}
