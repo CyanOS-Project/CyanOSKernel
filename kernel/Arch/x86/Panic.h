6f50dc5725307935ebb126fabcdbd0c62be72685
AymenSekhri <aymen.sekhri@live.fr>
2021-01-09 19:31:21 +0100

    Build: Remove travis CI.

--- kernel/Arch/x86/Panic.h
+++ kernel/Arch/x86/Panic.h
@@ -6,5 +6,6 @@
 #define PANIC(str)                                                                                                     \
 	do {                                                                                                               \
 		DISABLE_INTERRUPTS();                                                                                          \
+		err() << "PANIC: " << str << " " << __FILE__ << ":" << __LINE__;                                               \
 		HLT();                                                                                                         \
 	} while (0)
