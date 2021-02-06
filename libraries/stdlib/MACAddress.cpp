72268b87c0d53fc70f1c553f75032e945b1e40a6
Aymen Sekhri <Aymen.sekhri@live.fr>
2021-02-06 18:13:03 +0100

    Stdlib: New functionality to `MACAddress`.

--- libraries/stdlib/MACAddress.cpp
+++ libraries/stdlib/MACAddress.cpp
@@ -1,10 +1,22 @@
 #include "MACAddress.h"
 
+MACAddress MACAddress::Broadcast = MACAddress{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
 MACAddress::MACAddress(uint8_t* mac)
 {
 	for (size_t i = 0; i < MAC_ELEMENTS_SIZE; i++) {
 		m_mac[i] = mac[i];
 	}
+}
+
+MACAddress::MACAddress(uint8_t mac0, uint8_t mac1, uint8_t mac2, uint8_t mac3, uint8_t mac4, uint8_t mac5)
+{
+	m_mac[0] = mac0;
+	m_mac[1] = mac1;
+	m_mac[2] = mac2;
+	m_mac[3] = mac3;
+	m_mac[4] = mac4;
+	m_mac[5] = mac5;
 }
 
 MACAddress::MACAddress(const MACAddress& other)
@@ -44,4 +56,21 @@ MACAddress MACAddress::mask(const MACAddress& mask) const
 		new_mac.m_mac[i] = m_mac[i] & mask.m_mac[i];
 	}
 	return new_mac;
+}
+
+uint8_t& MACAddress::operator[](uint8_t index)
+{
+	return m_mac[index];
+}
+
+const uint8_t& MACAddress::operator[](uint8_t index) const
+{
+	return m_mac[index];
+}
+
+void MACAddress::copy(uint8_t* data) const
+{
+	for (size_t i = 0; i < MAC_ELEMENTS_SIZE; i++) {
+		data[i] = m_mac[i];
+	}
 }
--- libraries/stdlib/MACAddress.h
+++ libraries/stdlib/MACAddress.h
@@ -6,11 +6,17 @@ class MACAddress
   public:
 	MACAddress() = default;
 	MACAddress(uint8_t* mac);
+	MACAddress(uint8_t mac0, uint8_t mac1, uint8_t mac2, uint8_t mac3, uint8_t mac4, uint8_t mac5);
 	MACAddress(const MACAddress&);
 	MACAddress& operator=(const MACAddress&);
+	uint8_t& operator[](uint8_t index);
 	bool operator==(const MACAddress& mac) const;
 	bool operator!=(const MACAddress& mac) const;
 	MACAddress mask(const MACAddress&) const;
+	const uint8_t& operator[](uint8_t index) const;
+	void copy(uint8_t* data) const;
+
+	static MACAddress Broadcast;
 
   private:
 	static constexpr uint8_t MAC_ELEMENTS_SIZE = 6;
