997da14e5b39bcbc51cea0128e1c4076c80958d7
Aymen Sekhri <Aymen.sekhri@live.fr>
2021-03-01 21:40:37 +0100

    Stdlib: Added const operators in `Reference`.

--- libraries/stdlib/Reference.h
+++ libraries/stdlib/Reference.h
@@ -9,11 +9,16 @@ template <typename T> class Reference
 	DEFAULT_DESTRUCTOR(Reference)
 
 	Reference(T* storage) : m_storage{storage} { ASSERT(storage); }
-	Reference<T>::Reference(T& storage) : m_storage{&storage} {}
+	Reference(T& storage) : m_storage{&storage} {}
 	T& value() { return *m_storage; }
 	T* ptr() { return m_storage; }
 	T& operator*() { return *m_storage; }
 	T* operator->() { return m_storage; }
+
+	const T& value() const { return *m_storage; }
+	const T* ptr() const { return m_storage; }
+	const T& operator*() const { return *m_storage; }
+	const T* operator->() const { return m_storage; }
 
   private:
 	T* m_storage;
