ffb80d1e5327a570721a46a30839646898775d0c
Aymen Sekhri <Aymen.sekhri@live.fr>
2021-05-20 14:52:59 +0100

    Stdlib: Added `NullOrReference` container.

---
+++ libraries/stdlib/NullOrReference.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Assert.h"
+
+template <typename T> class NullOrReference
+{
+  public:
+	DEFAULT_COPY(NullOrReference)
+	DEFAULT_MOVE(NullOrReference)
+	DEFAULT_DESTRUCTOR(NullOrReference)
+
+	NullOrReference() { m_storage = nullptr; }
+	NullOrReference(T* storage) : m_storage{storage} { ASSERT(storage); }
+	NullOrReference(T& storage) : m_storage{&storage} {}
+	T& value()
+	{
+		ASSERT(is_valid());
+		return *m_storage;
+	}
+	T* ptr()
+	{
+		ASSERT(is_valid());
+		return m_storage;
+	}
+	T& operator*()
+	{
+		ASSERT(is_valid());
+		return *m_storage;
+	}
+	T* operator->()
+	{
+		ASSERT(is_valid());
+		return m_storage;
+	}
+
+	const T& value() const
+	{
+		ASSERT(is_valid());
+		return *m_storage;
+	}
+	const T* ptr() const
+	{
+		ASSERT(is_valid());
+		return m_storage;
+	}
+	const T& operator*() const
+	{
+		ASSERT(is_valid());
+		return *m_storage;
+	}
+	const T* operator->() const
+	{
+		ASSERT(is_valid());
+		return m_storage;
+	}
+	bool is_valid() const { return m_storage != nullptr; }
+
+  private:
+	T* m_storage;
+};
